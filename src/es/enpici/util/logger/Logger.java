460f09b6e41ba9a7345086cbdeb5bd29f95a4a39
enpici <er.kikillo.92@gmail.com>
2015-11-10 21:13:39 +0100

    The first commit

---
+++ src/es/enpici/util/logger/Logger.java
@@ -0,0 +1,121 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.enpici.util.logger;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Enrique
+ */
+public class Logger {
+    
+     private final String clase;
+    /**
+     * Constructor de la clase Logger.
+     * @param clase Parametro para especificar sobre que clase trabaja el Logger
+     */
+    public Logger(String clase) {
+        this.clase=clase;
+    }
+    /**
+     * Activa el Log
+     */
+    public void On() {
+        Log.LogOn();
+    }
+    /**
+     * Desactiva el Log
+     */
+    public void Off() {
+        Log.LogOff();
+    }
+    /**
+     * Muestra el estado del Log
+     * @return boolean (Activo/Desactivo)
+     */
+    public boolean State() {
+        return Log.LogState();
+    }
+    /**
+     * Genera un Log de tipo info.
+     * @param text 
+     */
+    public void info(String text){
+        this.writeLog(text, Log.INFO);
+    }
+    /**
+     * Genera un Log de tipo warning.
+     * @param text 
+     */
+    public void warning(String text){
+         this.writeLog(text, Log.WARNING);
+    }
+    /**
+     * Genera un Log de tipo severe.
+     * @param text 
+     */
+    public void severe(String text){
+         this.writeLog(text, Log.SEVERE);
+    }
+    /**
+     * Genera un Log del tipo especifico.
+     * @param text
+     * @param level Tipo de Log
+     */
+    public void writeLog(String text,Integer level) {
+        Log.writeLog(text,level,clase);
+    }
+    /**
+     * Genera un Log del tipo especifico.
+     * @param arraytext Array con textos a mostrar del mismo tipo
+     * @param level Tipo de Log
+     */
+    public void writeLog(String[] arraytext,Integer level) {
+        Log.writeLog(arraytext,level,clase);
+    }
+    /**
+     * Genera un Log del tipo especifico.
+     * @param arraytext ArrayList con textos a mostrar del mismo tipo
+     * @param level Tipo de Log
+     */
+    public  void writeLog(ArrayList<String> arraytext,Integer level) {
+        Log.writeLog(arraytext,level,clase);
+    }
+    /**
+     * Devuelve el log convertido en texto.
+     * @return String con el contenido del log
+     */
+    public  String toText() {
+       
+         return Log.toText();
+    }
+    /**
+     * Reinicia el log
+     */
+    public  void Restart() {
+        Log.Restart();
+    }
+    /**
+     * Escribe el fichero Log.
+     */
+    public  void WriteFile() {
+        Log.WriteFile();
+    }
+    /**
+     * como la funcion writeLog.
+     * @param level
+     * @param text 
+     */
+    public void log(Integer level, String text ) {
+      this.writeLog(text, level);
+    }
+
+    
+
+   
+   
+}
