91ee92c09e1f6008bc72a41825d8e09061bf59ae
SekthDroid <secthndamon@gmail.com>
2015-11-18 01:09:58 +0100

    Converted into Maven project
    The project wasn’t a maven standard project, I have refactored it to be
    able to provide a working maven project

---
+++ src/test/java/es/enpici/logger/LogTest.java
@@ -0,0 +1,13 @@
+package es.enpici.logger;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by SekthDroid on 18/11/15.
+ */
+public class LogTest extends TestCase {
+
+    public void testLogOn() throws Exception {
+        assertEquals(1, 1);
+    }
+}
